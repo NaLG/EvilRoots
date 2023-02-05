8c1c0218988aa9f7865f6bf15d49599eea7fab4a
atet <atet.kao@uci.edu>
2023-02-05 02:34:42 -0800

    Scrolling and spawning ground and obstacles.

---
+++ Assets/Scripts/MovingObjects.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovingObjects : MonoBehaviour
+{
+
+    [SerializeField] private float _speed;
+    [SerializeField] private float _xBound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position -= Vector3.right * _speed * Time.deltaTime;
+
+        if(this.transform.position.x < _xBound)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
